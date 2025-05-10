683c280596fcb95da5db592c321836a66941a6cf
bibo1299 <lukinih.d@yandex.ru>
2025-05-10 15:34:54 +0500

    Add files via upload

--- final.py
+++ final.py
@@ -5,8 +5,12 @@ class GameSprite(sprite.Sprite):
 
     def __init__(self, player_image,  player_x, player_y, size_x, size_y,player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.im=player_image
+        self.image = transform.scale(image.load(self.im), (size_x, size_y))
         self.speed = player_speed
+
+        self.size_x=size_x
+        self.size_y=size_y
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
@@ -35,18 +39,26 @@ class BLOCK(GameSprite):
         self.rect.x= t1.rect.x+self.sdig_x
         self.rect.y= t1.rect.y+self.sdig_y
 
+        if self.rect.y<400:
+            self.cppium()
+            self.kill()
+            
+    def cppium(self):
+        t9 = BLOCK(self.im, self.rect.x,self.rect.y, self.size_x, self.size_y, 0,0,0)
+        cop.add(t9)
+
 
 
         
-
+cop=sprite.Group()
 
 win_widht = 500
 win_height = 500
 kof=5
 display.set_caption('Блок баст')
 window = display.set_mode((win_widht, win_height))
-background = transform.scale(image.load("New Piskel.png"), (500, 500))
-t1 = BLOCK("New Piskel (8).png",350,400, 50, 50, 1)
+background = transform.scale(image.load("New Piskel (9).png"), (500, 500))
+
 
     #gamer = Player("unnamed.jpg",win_widht//2,win_height//2, 50, 50, 5)
 
@@ -59,32 +71,60 @@ mixer.init()
 #finish_sound.play()
 lini=sprite.Group()
 for i in range(7):
-    tt = BLOCK("New Piskel (10).png",100+50*i,50, 2, 300, 1)
+    tt = GameSprite("New Piskel (10).png",100+50*i,50, 2, 300, 1)
     lini.add(tt)
 for i in range(7):
-    tt = BLOCK("New Piskel (10).png",100,50+50*i, 300, 2, 1)
+    tt = GameSprite("New Piskel (10).png",100,50+50*i, 300, 2, 1)
     lini.add(tt)
+t1 = BLOCK('bgqwe.jpg', 100,500, 50, 50 ,0,0,0)
+blocks = sprite.Group()
+def create_ge(blocks):
+    global t1
+    t1 = BLOCK('bgqwe.jpg', 100,400, 50, 50 ,0,0,0)
+    blocks.add(t1)
+    t2 = BLOCK('New Piskel (8).png', t1.rect.x,t1.rect.y, 50, 50, 0,50,0)
+    blocks.add(t2)
+    t3 = BLOCK('New Piskel (8).png', t1.rect.x,t1.rect.y, 50, 50, 0,100,0)
+    blocks.add(t3)
+    t4 = BLOCK('New Piskel (8).png', t1.rect.x,t1.rect.y, 50, 50, 0,100,50)
+    blocks.add(t4)
+    blocks.update(t1)
+create_ge(blocks)
+
+def create_palki(blocks):
+    global t1
+    t1 = BLOCK('bgqwe.jpg', 100,400, 50, 50 ,0,0,0)
+    blocks.add(t1)
+    t2 = BLOCK('New Piskel (11).png', t1.rect.x,t1.rect.y, 50, 50, 0,50,0)
+    blocks.add(t2)
+    blocks.update(t1)
+
+def create_yy(blocks):
+    global t1
+    t1 = BLOCK('bgqwe.jpg', 100,400, 50, 50 ,0,0,0)
+    blocks.add(t1)
+    t2 = BLOCK('New Piskel (12).png', t1.rect.x,t1.rect.y, 50, 50, 0,50,0)
+    blocks.add(t2)
+    t3 = BLOCK('New Piskel (12).png', t1.rect.x,t1.rect.y, 50, 50, 0,100,0)
+    blocks.add(t3)
+    t4 = BLOCK('New Piskel (12).png', t1.rect.x,t1.rect.y, 50, 50, 0,100,50)
+    blocks.add(t4)
+    t5 = BLOCK('New Piskel (12).png', t1.rect.x,t1.rect.y, 50, 50, 0,100,100)
+    blocks.add(t5)
+    blocks.update(t1)
+
 
 u=1
 back = (80, 80, 80)
 run = True
-while run:
-    window.fill(back)
-    window.blit(background,(0,0))
-    lini.draw(window)
-    t1.reset()
-    for e in event.get():
-        keys = key.get_pressed()
-        if e.type == QUIT:
-            run = False
-        if e.type == MOUSEBUTTONDOWN and e.button == 1:
-            
-            x, y = e.pos
-            print(x,y,'lj')
-            if t1.rect.collidepoint(x,y):
+FPS = 60
+finish=False
+def drug(t1,x,y):
+    global blocks
+    if t1.rect.collidepoint(x,y):
                 if t1.grug==False:
                     t1.grug=True
-            elif t1.grug==True:
+    elif t1.grug==True:
                     t1.grug=False
                     if x%50<=45:
                         x-=x%50
@@ -97,6 +137,31 @@ while run:
                     print(x,y,'gckt')
                     t1.rect.x=x
                     t1.rect.y=y
+                    blocks.update(t1)
+                    k=randint(0,2)
+                    if k==0:
+                        create_ge(blocks)
+                    elif k==1:
+                        create_palki(blocks)
+                    elif k==2:
+                        create_yy(blocks)
+
+while run:
+    window.fill(back)
+    window.blit(background,(0,0))
+    lini.draw(window)
+    blocks.draw(window)
+    cop.draw(window)
+    
+    for e in event.get():
+        keys = key.get_pressed()
+        if e.type == QUIT:
+            run = False
+        if e.type == MOUSEBUTTONDOWN and e.button == 1:
+            
+            x, y = e.pos
+            drug(t1,x,y)
+
                 
                     
     display.update()
